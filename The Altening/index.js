4dd1016f8456ad4dde39c45bd8d6cc748cf64e22
DuckySoLucky <robertkovac160@gmail.com>
2022-07-02 09:30:09 +0200

    1.1
    Changes
    ・Multiple Account Support

--- The Altening/index.js
+++ The Altening/index.js
@@ -1,35 +1,42 @@
 const mineflayer = require('mineflayer');
 const config = require('./config.json');
-const { Client, Intents } = require("discord.js");
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const { MessageEmbed, Client, Intents } = require("discord.js");
+const client = new Client({intents: new Intents(32767)})
+let bot;
 
-const bot = mineflayer.createBot({
-    authServer: 'http://authserver.thealtening.com',
-    sessionServer: 'http://sessionserver.thealtening.com',
-    host: config.server.host,
-    port: config.server.port,
-    username: config.minecraft.username,
-    password: 'DuckySoLucky'
+client.once('ready', () => {
+    console.log('Discord > Client ready, logged in as ' + client.user.tag);
+    start();
 })
 
-function wait(seconds) {
-    var waitTill = new Date(new Date().getTime() + seconds * 1000)
-    while (waitTill > new Date()) {}
-}
-
-bot.on('login', () => {
-    while (1==1) {
-        bot.chat(`/ad ${config.minecraft.serverName} ${config.minecraft.adMessage}`)
-        console.log(`/ad ${config.minecraft.serverName} ${config.minecraft.adMessage}`)
-        if (config.discord.customMessage) {
-            client.channels.cache.get(config.discord.channel).send(`<@${config.discord.userId}> ${bot.username} advertised ${config.minecraft.serverName}`)
+function start() {
+    const username = config.minecraft.username
+    setInterval(() => {
+        for (let i = 0; i < username.length; i++) {
+            bot = mineflayer.createBot({
+                authServer: 'http://authserver.thealtening.com',
+                sessionServer: 'http://sessionserver.thealtening.com',
+                host: config.server.host,
+                port: config.server.port,
+                username: username[i],
+                password: 'DuckySoLucky'
+            })
+            bot.on('login', () => {
+                bot.chat(`/ad ${config.minecraft.serverName} ${config.minecraft.adMessage}`)
+                console.log(`Minecraft > ${bot.username}:  ${config.minecraft.serverName} ${config.minecraft.adMessage}`)
+                if (config.discord.customMessage) {
+                    const adEmbed = new MessageEmbed()
+                        .setColor('#00FF00')
+                        .setAuthor({ name: `${bot.username}`})
+                        .setDescription(`${config.minecraft.serverName}\n${config.minecraft.adMessage}`)
+                        .setFooter({ text: config.embed, iconURL: 'https://cdn.discordapp.com/avatars/486155512568741900/31cabcf3c42823f8d8266fd22babb862.png?size=4096' });
+                    client.channels.cache.get(config.discord.channel).send({embeds: [ adEmbed ]})
+                }
+            })
+            bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
+            setInterval(() => {}, config.minecraft.waitBeforeLogging*1000)
         }
-        wait(config.minecraft.cooldown) 
-    }
-})
-
-client.on("ready", () => {console.log('Discord > Client ready, logged in as ' + client.user.tag);});
-bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
-bot.on('spawn', () => console.log('Minecraft > Client ready, logged in as ' + bot.username))
+    }, (config.minecraft.waitBeforeLogging*1000*5));
+}
 
 client.login(config.discord.token);
